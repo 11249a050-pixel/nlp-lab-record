4cdc4031bb45ecfdbd1be162152b1169de8efce1
11249a050-pixel <11249a050@kanchiuniv.ac.in>
2026-09-24 16:06:41 +0530

    Add files via upload

---
+++ exp3.py
@@ -0,0 +1,7 @@
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+ps = PorterStemmer()
+new_text = "is is very important to be pythonly while you are pythoning with python.All pythoners have poorly pythoned atleast once"
+words = word_tokenize(new_text)
+for w in words:
+    print(ps.stem(w))
